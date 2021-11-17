54e8e19be1aee9845f98079e84da6c7785a8cd70
Marc <marc.artco@gmail.com>
2021-11-17 09:01:10 +0100

    Atributs i primer mètode creat

---
+++ nivell 1/ex1.php
@@ -0,0 +1,26 @@
+<?php
+
+
+    class Employee{
+        //atributs de classe
+        public $nom;
+        public $sou;
+
+        //mètodes de classe. Paràmetres no i sou
+        public function initialize($nombre,$sueldo){
+            $this->nom=$nombre;
+            $this->sou=$sueldo;
+        }   
+
+        
+
+
+
+
+
+    }
+
+
+
+
+?>
