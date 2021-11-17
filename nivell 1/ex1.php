877f15cd34732f6987f54d18c6017829cdd8309e
Marc <marc.artco@gmail.com>
2021-11-17 09:49:03 +0100

    ex1 en curs

--- nivell 1/ex1.php
+++ nivell 1/ex1.php
@@ -1,25 +1,35 @@
 <?php
 
 
+
     class Employee{
         //atributs de classe
         public $nom;
-        public $sou;
+        // public $sou;
 
-        //mètodes de classe. Paràmetres no i sou
-        public function initialize($nombre,$sueldo){
+        //mètodes de classe. Paràmetres nom i sou
+        public function initialize($nombre){
             $this->nom=$nombre;
-            $this->sou=$sueldo;
-        }   
+            // $this->sou=$sueldo;
+        }
+        //imprimir el nom i un misstage per saber si ha de pagar impostos (si sou>6000 paga impostos)
+        public function print(){
+            echo $this->nom;
+            echo '<br>';
+           
+        }
 
         
-
-
-
-
-
     }
-
+    
+    //creació i assignació d'objectes
+ 
+    $treb1=new Employee();
+    $treb1->initialize('Joan');
+    $treb1->print();
+    
+    // $treb1->initialize('Joan','12000');
+    // $treb1->print();
 
 
 
