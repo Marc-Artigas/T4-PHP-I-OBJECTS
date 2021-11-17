47d11ce1c5ffd9c4b2027ebf82cddd109c69ba8e
Marc <marc.artco@gmail.com>
2021-11-17 10:13:29 +0100

    ex1 finalitzat

--- nivell 1/ex1.php
+++ nivell 1/ex1.php
@@ -4,18 +4,25 @@
 
     class Employee{
         //atributs de classe
-        public $nom;
-        // public $sou;
+        private $nom;
+        private $sou;
 
         //mètodes de classe. Paràmetres nom i sou
-        public function initialize($nombre){
-            $this->nom=$nombre;
-            // $this->sou=$sueldo;
+        public function initialize($nom,$sou){
+            $this->nom=$nom;
+            $this->sou=$sou;
         }
         //imprimir el nom i un misstage per saber si ha de pagar impostos (si sou>6000 paga impostos)
         public function print(){
-            echo $this->nom;
+            echo "Treballador: ".$this->nom."."." Cobra ".$this->sou."€. "; 
+                  
+            if($this->sou>6000){
+                echo "Ha de pagar impostos.";
+            } else {
+                echo "No ha de pagar impostos.";
+            }
             echo '<br>';
+            
            
         }
 
@@ -25,11 +32,14 @@
     //creació i assignació d'objectes
  
     $treb1=new Employee();
-    $treb1->initialize('Joan');
+    $treb1->initialize('Joan','12000');
     $treb1->print();
+
+    $treb2=new Employee();
+    $treb2->initialize('David','5800');
+    $treb2->print();
     
-    // $treb1->initialize('Joan','12000');
-    // $treb1->print();
+    
 
 
 
