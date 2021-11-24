abf0687df013bcce4b902d76a72bdcb2f41c6aa4
Marc <marc.artco@gmail.com>
2021-11-24 14:33:13 +0100

    nivell3 falta formulari

--- nivell 3/main.php
+++ nivell 3/main.php
@@ -7,7 +7,7 @@
         public $saldoActual;
         //constructor
 
-    }
+    
         function __construct($numCompte,$nom,$cognom,$saldoActual){
             $this->numCompte=$numCompte;
             $this->nom=$nom;
@@ -15,9 +15,41 @@
             $this->saldoActual=$saldoActual;
         }
 
-        
+        public function deposit($importIngrés){
+            echo "El saldo inicial del client és de ".$this->saldoActual."€."."<br>";
+            $this->saldoActual=$this->saldoActual+$importIngrés;
+            echo "El client ".$this->nom." ".$this->cognom." ha ingressat ".$importIngrés."€. Actualment té un saldo de ".$this->saldoActual."€."."<br>";
+        }
 
+        public function withdraw($importRetirar){   
+            echo "El saldo inicial del client és de ".$this->saldoActual."€."."<br>";      
+                if($this->saldoActual>$importRetirar){
+                    $this->saldoActual=$this->saldoActual-$importRetirar;
+                    echo "El client ".$this->nom." ".$this->cognom." ha retirat ".$importRetirar."€. Actualment té un saldo de ".$this->saldoActual."€."."<br>";
+            } else {
+                    echo "L'import sol·licitat de ".$importRetirar."€, és superior al saldo disponible."."<br>";
+            }
+        }
 
+    }
+
+        //objectes
+        $client1 = new Account ('ES001', 'Pol', 'Garcia', '10');
+        $client1 -> deposit(1);
+        $client1 -> deposit(4);
+        $client1 -> deposit(4);
+        $client1 -> withdraw(1);
+        $client1 -> withdraw(4);
+
+        $client2 = new Account ('ES002','Toni','Pagès','25');
+        $client2 -> deposit(1);
+        $client2 -> deposit(4);
+        $client2 -> deposit(4);
+        $client2 -> withdraw(1);
+        $client2 -> withdraw(4);
+        $client2 -> withdraw(40);
+        $client2 -> deposit(5);
+        $client2 -> withdraw(40);
 
 
 
